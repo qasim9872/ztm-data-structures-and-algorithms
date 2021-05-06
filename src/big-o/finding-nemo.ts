2c35b7bcbea5271a3bde7a2a6b7058866b45d986
Muhammad Qasim <qasim9872@gmail.com>
2021-05-06 18:17:08 +0100

    added find nemo - O(n)

--- src/big-o/finding-nemo.ts
+++ src/big-o/finding-nemo.ts
@@ -1,19 +1,12 @@
-import { performance } from "perf_hooks"
-
-const nemo = ["nemo", ...new Array(100000).fill("nemo")]
+const nemo = ["nemo", ...new Array(100).fill("nemo")]
 
 function findNemo(array: string[]) {
-    const t0 = performance.now()
-
+    // Runtime: O(n) --> Linear Time
     for (let i = 0; i < array.length; i++) {
         if (array[i] === "nemo") {
             console.log("Found NEMO")
         }
     }
-
-    const t1 = performance.now()
-
-    console.log(`Call to find Nemo took ${t1 - t0} milliseconds`)
 }
 
 findNemo(nemo)
